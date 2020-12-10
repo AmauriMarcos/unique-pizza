f9bebb3a90d06157ca9b0cc20cf1f8184950462c
AmauriMarcos <amaurisantos.m@gmail.com>
2020-12-10 13:35:25 +0100

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+   <section id="navbar">
+       <nav class="nav">
+           <ul class="nav__list">
+               <li class="nav__list-item">
+                   <a href="#">Home</a>
+               </li>
+           </ul>
+       </nav>
+   </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+    .nav{
+        width: 100%;
+
+        &__list{
+         
+
+            &-item{
+                
+
+            }
+        }
+    }
+</style>>
+
+
